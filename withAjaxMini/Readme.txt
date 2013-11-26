3f93ce1c009f6d1310fea0c4c831ca923a922db8
irvingdp <irvingdp@gmail.com>
2013-11-26 18:08:24 +0800

    init ver1

---
+++ withAjaxMini/Readme.txt
@@ -0,0 +1,14 @@
+﻿1.setup Microsoft Ajax Minifier 
+	http://ajaxmin.codeplex.com/
+
+2.setup power shell
+	http://support.microsoft.com/kb/968929/en-us
+
+3.execution "Set-ExecutionPolicy RemoteSigned" under powershell(run as administrator)
+
+4.add "ajaxminifier.ps1" to project
+	edit ajaxminifier.ps1 -> 
+	$execPath="C:\Program Files (x86)\Microsoft\Microsoft Ajax Minifier\ajaxmin.exe" //change your ajaxmin.exe current path.
+
+5.add below command to post-build-event
+	PowerShell $(ProjectDir)ajaxminifier.ps1 -ConfigurationName $(ConfigurationName) -ProjectDir $(ProjectDir)
---
+++ withAjaxMini/ajaxminifier.ps1
@@ -0,0 +1,64 @@
+﻿param($configurationName,$projectDir)
+$execPath="C:\Program Files (x86)\Microsoft\Microsoft Ajax Minifier\ajaxmin.exe"
+
+if($configurationName -eq  "release")
+{
+    #compass js file and backup origin file to *.js.org
+	$jsFileList = dir "$projectDir" -include *.js -recurse
+	 foreach ($file in $jsFileList)
+	 {      
+			if($file)
+			{	
+				$backupFilename = "$file" + ".org"
+				$backupFileExists = Test-Path "$backupFilename"
+				if(!$backupFileExists)
+				{
+					Copy $file $backupFilename
+					& $execPath -h -js "$file" -o "$file" -clobber
+				}
+			}
+	 }
+
+	 #compass css file and backup origin file to *.js.org
+	 $cssFileList = dir "$projectDir" -include *.css -recurse
+	 foreach ($file in $cssFileList)
+	 {      
+			if($file)
+			{	
+				$backupFilename = "$file" + ".org"
+				$backupFileExists = Test-Path "$backupFilename"
+				if(!$backupFileExists)
+				{
+					Copy $file $backupFilename
+					& $execPath -h -js "$file" -o "$file" -clobber
+				}
+			}
+	 }
+ }
+ else
+ {
+     #recover compass js file ( *.js.org -> *.js)
+	 $jsFileList = dir "$projectDir" -include *.js.org -recurse
+	 foreach ($backupFile in $jsFileList)
+	 {
+		if($backupFile) # if not null
+		{			
+			$fileFullPath = $backupFile.DirectoryName + '\' + $backupFile.BaseName			
+			Copy  $backupFile $fileFullPath
+			Del $backupFile
+		}
+	 }
+
+	 #recover compass css file ( *.css.org -> *.css)
+	 $cssFileList = dir "$projectDir" -include *.css.org -recurse
+	 foreach ($backupFile in $cssFileList)
+	 {
+		if($backupFile) # if not null
+		{			
+			$fileFullPath = $backupFile.DirectoryName + '\' + $backupFile.BaseName			
+			Copy  $backupFile $fileFullPath
+			Del $backupFile
+		}
+	 }
+ }
+
